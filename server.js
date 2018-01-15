d482fef8eef04caccf43f37268c470619c8d85dd
Paul Bausch <pbausch@gmail.com>
2018-01-15 13:56:29 -0800

    slim down a bit

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 var express = require('express');
-var fs = require('fs');
-var rp = require('request-promise');
-var cheerio = require('cheerio');
 var RSS = require('rss');
 var url = require('url');
 var twitter = require('twitter');
